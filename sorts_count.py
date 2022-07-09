595969c523c97ffeed0c6f6e1c9041c7f4754b8f
Eric Daily <dailye@oregonstate.edu>
2022-07-09 18:44:39 -0400

    Initial commit

---
+++ sorts_count.py
@@ -0,0 +1,36 @@
+def bubble_count(list):
+
+    comparisons = 0
+    exchanges = 0
+    length = len(list) - 1
+
+    for pass_num in range(length):
+        for index in range(length - pass_num):
+            comparisons += 1
+            if list[index] > list[index + 1]:
+                temp = list[index]
+                list[index] = list[index + 1]
+                list[index + 1] = temp
+                exchanges += 1
+    both = (comparisons, exchanges)
+    return both
+
+
+def insertion_count(list):
+
+    comparisons = 0
+    exchanges = 0
+
+    for i in range(1, len(list)):
+        j = i
+        comparisons += 1
+        while list[j - 1] > list[j] and j > 0:
+            temp = list[j-1]
+            list[j - 1] = list[j]
+            list[j] = temp
+            exchanges += 1
+            j -= 1
+
+    both = (comparisons, exchanges)
+    return both
+
